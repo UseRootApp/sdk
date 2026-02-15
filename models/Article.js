977af5870e190bcddfefffa5a4e56603d03f94fe
UseRootApp <rogermiya35@gmail.com>
2026-02-15 10:39:17 +0700

    Create Article.js

---
+++ models/Article.js
@@ -0,0 +1,8 @@
+export class Article {
+  constructor(id, title, publisher, content) {
+    this.id = id;
+    this.title = title;
+    this.publisher = publisher;
+    this.content = content;
+  }
+}
